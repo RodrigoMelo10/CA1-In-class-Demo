4b948e12038a2a364109e9b76ca86a262eb07f0f
RodrigoMelo10 <digoliveira10@yahoo.com.br>
2019-12-04 15:33:05 +0000

    adding functionalities to add

--- app.js
+++ app.js
@@ -10,6 +10,24 @@ var server = http.createServer(router);
 
 router.use(express.static(path.resolve(__dirname, 'views')));
 
+
+// Function to read in XML file and convert it to JSON
+function xmlFileToJs(filename, cb) {
+  var filepath = path.normalize(path.join(__dirname, filename));
+  fs.readFile(filepath, 'utf8', function(err, xmlStr) {
+    if (err) throw (err);
+    xml2js.parseString(xmlStr, {}, cb);
+  });
+}
+
+//Function to convert JSON to XML and save it
+function jsToXmlFile(filename, obj, cb) {
+  var filepath = path.normalize(path.join(__dirname, filename));
+  var builder = new xml2js.Builder();
+  var xml = builder.buildObject(obj);
+  fs.writeFile(filepath, xml, cb);
+}
+
 router.get('/', function(req, res){
 
     res.render('index');
@@ -32,6 +50,31 @@ router.get('/get/html', function(req, res) {
 
 
 });
+// POST request to add to JSON & XML files
+router.post('/post/json', function(req, res) {
+
+  // Function to read in a JSON file, add to it & convert to XML
+  function appendJSON(obj) {
+    console.log(obj);
+    // Function to read in XML file, convert it to JSON, add a new object and write back to XML file
+    xmlFileToJs('shop.xml', function(err, result) {
+      if (err) throw (err);
+      result.cafemenu.section[obj.sec_n].entree.push({'item': obj.item, 'price': obj.price});
+      console.log(result);
+      jsToXmlFile('shop.xml', result, function(err) {
+        if (err) console.log(err);
+      })
+    })
+  }
+
+  // Call appendJSON function and pass in body of the current POST request
+  appendJSON(req.body);
+
+  // Re-direct the browser back to the page, where the POST request came from
+  res.redirect('back');
+
+});
+
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
   var addr = server.address();
